ff881f274bb990f870684c7a65c145c39f216636
preetaulakhUS <preetaulakh.us@gmail.com>
2026-08-25 00:38:32 -0700

    Update 1.1
    - Updated Global README.md to add additional project information.
    - Updated hello_world.cpp to include a rough template of the project.

--- module01_setup/hello_world.cpp
+++ module01_setup/hello_world.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main()
 {
-	cout<<"hello world!\n";cout<<"this is a test.\n";
+	cout<<"\nHello World!\nWelcome to Account Nexus!\n\n";
+
+	// menu template
+	cout<<"\t\tMenu\n"<<string(38,'-')<<"\n";
+	cout<<"  i)\tTBD\n ii)\tTBD\niii)\tTBD\n iv)\tTBD\n  v)\texit";
+	cout<<"\n->";
+	cout<<"\n"<<string(38,'-');
 }
